dc8104dc185ff4cd8bd8c8e8730e251db8e8e816
wo-000 <2535834917@qq.com>
2022-03-23 14:04:51 +0800

    项目初始

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <el-button>fa</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+});
+</script>
