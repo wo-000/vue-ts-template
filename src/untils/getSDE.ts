4a91307bf3120a37c4ce77128711fe95b217c72d
wo-000 <2535834917@qq.com>
2022-03-26 11:27:22 +0800

    环境变量配置

---
+++ src/untils/getSDE.ts
@@ -0,0 +1,6 @@
+const sde = process.env.NODE_ENV; //是否为生产环境
+const isProduction = ["production", "prod"].includes(sde as string); //是否为生产环境
+const isFat = ["fat", "uat"].includes(sde as string); //是否为测试环境
+const isDevelopment = ["development", "dev"].includes(sde as string); //是否为开发环境
+
+export { isProduction, isFat, isDevelopment };
